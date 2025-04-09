7dabc2fb5c8d0fce956a2403cba86acef1e3fd22
AdityaGoswami01 <adityagoswamii01@gmail.com>
2025-04-09 18:11:54 +0530

    Commit

--- assests/js/shopping.js
+++ assests/js/shopping.js
@@ -105,13 +105,13 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCart();
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navLinks = document.getElementById("navLinks");
+  document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
   
     if (hamburger && navLinks) {
-      hamburger.addEventListener("click", function () {
-        navLinks.classList.toggle("show");
+      hamburger.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
       });
     }
   });
